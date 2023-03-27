5ec83c0750b579b25be0f71e7e6a06fc55bfd116
Ritik Prajapat <85746731+ritikprajapat@users.noreply.github.com>
2023-03-27 21:42:10 +0530

    Update AutomaticTrader.js

--- src/Pages/AutomaticTrader.js
+++ src/Pages/AutomaticTrader.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 function AutomaticTraderPage() {
   useEffect(() => {
     window.location.replace(
-      "https://shubham7204-final-year-cpp-project-crypto-main-mcgwdf.streamlit.app/"
+      #Link
     );
   });
   return () => useEffect;
