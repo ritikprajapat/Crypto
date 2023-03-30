28c63047bb009ca985e5fb89750040fb9ea81966
Ritik Prajapat <85746731+ritikprajapat@users.noreply.github.com>
2023-03-30 20:52:24 +0530

    link

--- src/Pages/PricePrediction.js
+++ src/Pages/PricePrediction.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 function PricePredictionPage() {
   useEffect(() => {
     window.open(
-      "https://shubham7204-final-year-cpp-project-crypto-main-mcgwdf.streamlit.app/",
+      "https://cryptomind-predictor.streamlit.app/",
       "_blank"
     );
   }, []);
